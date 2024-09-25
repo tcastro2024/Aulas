975bdf6d935d858446d85a888c5a4ce1d24fc289
Tiago Castro <tiagocastro044@gmail.com>
2024-09-25 07:30:17 +0100

    Aula 1 - Basico

--- Aula_0/Program.cs
+++ Aula_0/Program.cs
@@ -6,14 +6,27 @@ using System.Threading.Tasks;
 
 namespace Aula_0
 {
+    /*
+    * LEIM: POO
+    * tcastro
+    * 
+    * Soluções e Projetos
+    * Wizards
+    * Regiões
+    * Documentação de Código
+    * */
     class Program
     {
+        #region UM
         static void Main(string[] args)
         {
+            //Criar Soluções e Projetos
             Console.WriteLine("Hello world! Changed by git.");
             Console.WriteLine("Double hello world! Changed by git.");
             Console.WriteLine("Triple hello world! Changed by git.");
+
             Console.ReadKey();
         }
+        #endregion
     }
 }
