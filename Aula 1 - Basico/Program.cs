155f3f3095ac7be971c75437955ae25183fe3900
Tiago Castro <tiagocastro044@gmail.com>
2024-09-25 08:24:10 +0100

    Aula 1 - Comando Console.WriteLine

--- Aula 1 - Basico/Program.cs
+++ Aula 1 - Basico/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace Aula_1___Basico
 {
+    /*
+      * LEIM: POO
+      * tcastro
+      * 
+      * Soluções e Projetos
+      * Wizards
+      * Regiões
+      * Documentação de Código
+    * */
     internal class Program
     {
+        #region UM
         static void Main(string[] args)
         {
+            //Criar Soluções e Projetos
+            Console.WriteLine("Hello world!");
+
+            Console.ReadKey();
         }
+        #endregion
     }
 }
