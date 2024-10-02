0f1b8502547c42e13b6fd242b62440642fea680c
Tiago Castro <tiagocastro044@gmail.com>
2024-10-02 17:36:02 +0100

    aula 2 - Exemplo Notas POO

--- Aula 2 - Basico/Program.cs
+++ Aula 2 - Basico/Program.cs
@@ -2,7 +2,7 @@
 //    <copyright file="Aula_1.cs"    company="IPCA">
 //     Copyright (c) IPCA-EST 2024. All rights reserved.
 //    </copyright>
-//    <date>2024-09-26</date>
+//    <date>2024-10-02</date>
 //    <time>16:01</time>
 //    <version>0.1</version>
 //    <author>tcastro</author>
@@ -28,17 +28,43 @@ namespace Aula_2___Basico
              * 
              * 
              * 
-             
-            
+        
             Coment1
             Coment2
             */
 
-            Console.WriteLine("Aula 2");
-            Console.WriteLine("Aula 2");
+            //POO average = (CT * 0,4) + (CP * 0,6)
+
+            // int     Exemplo: idade, 1, 10, ...
+            // double  Exemplo: comprimento, 25,5 m
+            // string  Exemplo: "Aula POO é isto"
+            // bool    Exemplo: if(ETarde) true/false
+
+            double CT = 0.0, CP = 0.0, average = 0.0;
+
+            Console.Write("CT: ");
+            CT = double.Parse(Console.ReadLine());
+
+            Console.Write("CP: ");
+            CP = double.Parse(Console.ReadLine());
+
+            average = (CT * 0.4) + (CP * 0.6);
+
+            Console.WriteLine("NF: " + Math.Round(average, 2));
+
+            while (true)
+            {
+                Console.Write(' ');
+            }
+
 
     
-           
+
+                
+
+
+
+
             Console.ReadKey();
         }
     }
