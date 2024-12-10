3a010a3339d18aae8ce46df14e699ba863cd5c80
Tiago Castro <tiagocastro044@gmail.com>
2024-12-10 21:14:45 +0000

    Aula 20 - Revisões (continuação 1)

--- Aula 20 - Revisoes/Program.cs
+++ Aula 20 - Revisoes/Program.cs
@@ -10,15 +10,21 @@ namespace Aula_20___Revisoes
     {
         static void Main(string[] args)
         {
-           Estudante estudante = new Estudante("Andre", new ContaBancaria());
-           Estudante estudante1 = new Estudante("Ana", new ContaBancaria());
-           Estudante estudante2 = new Estudante("Paulo", new ContaBancaria());
+            Estudante estudante = new Estudante("Andre", new ContaBancaria());
+            Estudante estudante1 = new Estudante("Ana", new ContaBancaria());
+            Estudante estudante2 = new Estudante("Paulo", new ContaBancaria());
 
-           List<Estudante> estudantes = new List<Estudante>();
+            List<Estudante> estudantes = new List<Estudante>();
 
             estudantes.Add(estudante);
             estudantes.Add(estudante1);
             estudantes.Add(estudante2);
+
+            foreach (Estudante cadaEstudante in estudantes)
+            {
+                if (cadaEstudante.Nome.StartsWith("A"))
+                    Console.WriteLine(cadaEstudante.Nome);
+            }
 
 
         }
